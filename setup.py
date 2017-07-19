994839fa00c26937a49ec6b18140c8d3fc224f06
Marco Rossi <developer@marco-rossi.com>
2017-07-19 21:46:36 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bibtex-optimizer',
+      version='0.1',
+      packages=['bibtex-optimizer'],
+      author='Marco Rossi',
+      author_email='developer@marco-rossi.com',
+      url='https://github.com/m-rossi/bibtex-optimizer'
+      )
